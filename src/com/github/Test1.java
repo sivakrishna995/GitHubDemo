4851834d129eb6832982590a6ada6363da0641cd
Sony <Sony@DESKTOP-NKR1FA7>
2021-06-07 18:24:39 +0530

    First Commit

---
+++ src/com/github/Test1.java
@@ -0,0 +1,24 @@
+package com.github;
+
+public class Test1 {
+	
+	
+
+		public static void main(String[] args) {
+			
+			String hungry="yes";
+			
+			if(hungry=="yes") {
+				System.out.println("eat Something");
+				
+			}
+			else {
+				System.out.println("Do Work");
+			}
+
+		}
+
+	}
+
+
+
